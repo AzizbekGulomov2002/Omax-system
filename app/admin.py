1c21917f0c4a95f930f1afa0821d62c1d1a212c8
Azizbek <azizbekgulomov>
2023-01-04 08:08:25 +0500

    added-summ-field

--- app/admin.py
+++ app/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from .models import Buyurtma, Import, Export, Mijoz, Ombor, Hodim
 
 class ImportAdmin(admin.ModelAdmin):
-    list_display = ['mahsulot_nomi','format','import_vaqt','miqdor','narx']
+    list_display = ['mahsulot_nomi','format','import_vaqt','miqdor','narx','summa']
     list_filter = ('mahsulot_nomi','format','import_vaqt','miqdor','narx')
     list_per_page = 10
     search_fields = ['mahsulot_nomi','format','import_vaqt','miqdor','narx']
@@ -19,7 +19,7 @@ admin.site.register(Import, ImportAdmin)
 
 
 class ExportAdmin(admin.ModelAdmin):
-    list_display = ['mahsulot_nomi','format','export_vaqt','miqdor','narx']
+    list_display = ['mahsulot_nomi','format','export_vaqt','miqdor','narx','summa']
     list_filter = ('mahsulot_nomi','format','export_vaqt','miqdor','narx')
     list_per_page = 10
     search_fields = ['mahsulot_nomi','format','export_vaqt','miqdor','narx']
@@ -43,7 +43,7 @@ admin.site.register(Mijoz, MijozAdmin)
 
 class BuyurtmaAdmin(admin.ModelAdmin):
     list_filter = ('format','buyurtma_sana','miqdor','narx')
-    list_display = ['mijoz','format','buyurtma_sana','miqdor','narx','summa']
+    list_display = ['mijoz','format','buyurtma_sana','miqdor','narx','summa','summa']
     list_per_page = 10
     search_fields = ['mijoz','format','buyurtma_sana','summa']
 
